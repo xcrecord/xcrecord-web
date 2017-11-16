ec243974a661558b07efc254003e01641f6bdd40
pansinm <pansinm@foxmail.com>
2017-11-16 20:33:29 +0800

    [feature] 引入antd-mobile

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
 import * as ReactDOM from "react-dom";
+import { Button } from 'antd-mobile';
 import App from './App';
 
 ReactDOM.render(
-  <App/>,
+  <Button/>,
   document.getElementById('content')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const autoprefixer = require('autoprefixer');
+const pxtorem = require('postcss-pxtorem');
+
 module.exports = {
+  devtool: "source-map",
+  devServer: {
+    disableHostCheck: true
+  },
+
   entry: "./src/index.tsx",
   output: {
-      filename: "bundle.js",
-      path: __dirname + "/dist"
+    filename: "bundle.js",
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/dist/'
   },
-
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
 
   resolve: {
-      // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: [".ts", ".tsx", ".js", ".json"]
+    modulesDirectories: ['node_modules', path.join(__dirname, '../node_modules')],
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
   },
-
   module: {
-      rules: [
-          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-          { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
-          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-      ]
+    loaders: [
+      // 建议不使用 awesome-typescript-loader，否则自己设置
+      // { test: /\.tsx?$/, loader: "babel-loader!awesome-typescript-loader?instance=ts-loader" }
+      { test: /\.tsx?$/, loaders: [require.resolve('babel-loader'), require.resolve('ts-loader')] },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap!postcss-loader') },
+      { test: /\.less$/, loader: ExtractTextPlugin.extract('css?sourceMap!postcss-loader!less?sourceMap') },
+      { test: /\.(jpg|png)$/, loader: "url?limit=8192" }, //把不大于8kb的图片打包处理成Base64
+    ],
+    preLoaders: [
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: "source-map-loader" }
+    ]
   },
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
+  postcss: [
+    autoprefixer({
+      browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8', 'iOS >= 8', 'Android >= 4'],
+    }),
+    // pxtorem({ rootValue: 100, propWhiteList: [] })
+  ],
   externals: {
-      "react": "React",
-      "react-dom": "ReactDOM"
+    "react": "React",
+    "react-dom": "ReactDOM"
   },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('common.js'),
+    new ExtractTextPlugin('bundle.css', { disable: false, allChunks: true }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+  ],
 };
